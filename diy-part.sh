2a16d171be5a2d5f73aebd5f1e1135cfbd417797
jtzx082 <yujq804@gmail.com>
2025-12-06 15:56:38 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-# ---------------------------------------------------------
-# File: diy-part.sh
-# Description: 用于在 SDK 更新 feed 之前/之后添加自定义插件源码
-# ---------------------------------------------------------
-
-# 进入 package 目录 (SDK 目录下)
+# 进入 package 目录
 cd package
 
-echo ">>> Cloning custom repositories..."
-
-# 1. Nikki (OpenWrt-nikki)
+# 1. Nikki (聚合仓库，包含核心和界面)
+# 注意：直接 clone 即可，后续编译脚本会自动找里面的 luci-app-nikki
 git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
 
 # 2. DDNS-Go
 git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git
 
-# 你可以在这里继续添加其他 git clone 命令
-# git clone https://github.com/xxx/xxx.git
-
-# 返回上级目录
+# 返回上级
 cd ..
-
-echo ">>> Custom repositories cloned."
